83825d912144b58a2d99907fbc908337c0d23d2b
Nitin Jaryal <Nitin_Jaryal@epam.com>
2019-05-23 19:41:33 +0530

    Import Export

---
+++ src/Sports.js
@@ -0,0 +1,24 @@
+export class Sports {
+    constructor(name, players) {
+        this.name = name;
+        this.players = players;
+    }
+
+    getName() {
+        alert(`${this.name} has ${this.players} players`);
+    }
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app.js
@@ -0,0 +1,4 @@
+import {Sports} from './Sports.js'
+const TT = new Sports("Maneesh", 4)
+console.log(TT.getName())
+
